96293fdcca701627f10ff03a939179285321c9d1
parveenhumm <humairaparveen474@gmail.com>
2024-04-27 23:43:07 +0500

    name change

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
-#! /usr/bin/env node
-import inquirer from "inquirer";
+#! /usr/bin/env node //Shabang ("Shabang" is a way to declare the interpreter to be used when executing a script)
+//indicating that the script should be executed using the node interpreter.
+import inquirer from "inquirer"; //importing inquirer module
 const randomNumber = Math.floor(Math.random() * 4 + 1);
+//math function floor will round off number
+// random will generate the random number
 const answers = await inquirer.prompt([
     {
-        "name": "UserGuessedNumber",
-        "type": "number",
-        "message": "Please guess a number",
+        "name": "UserGuessedNumber", //name of variable to store input
+        "type": "number", // type of input
+        "message": "Please guess a number", // message displayed to user
     },
 ]);
 if (answers.UserGuessedNumber === randomNumber) {
     console.log("congratulation you Guessed right number");
+    // when input is eaqual to random number
 }
 else {
     console.log("you Guessed wrong number");
+    // when input is not equal to random number
 }
